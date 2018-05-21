9f90ce9dbeb1eebdf135c838ff8500b76bc1492e
LeeviT <leevi.tuikka@helsinki.fi>
2018-05-21 20:41:35 +0300

    tehtävänannot

--- main.f95
+++ main.f95
@@ -9,7 +9,8 @@ program main
   allocate(g_matrix(n, n))
   g_matrix = 0
 
-  call apply_bound_conditions(n, g_matrix, 1.0, 1.0, 1.0, 1.0)
+  call apply_bound_conditions(n, g_matrix, 0.0, 0.0, 0.0, 0.0)
+  call build_g_matrix(n, g_matrix)
 
   write (*,*) g_matrix
 
@@ -32,9 +33,23 @@ contains
   end subroutine apply_bound_conditions
 
   subroutine build_g_matrix(n, g_matrix)
-    integer :: n
+    integer :: n, i, j
     real, dimension(n, n) :: g_matrix
+    real(8) :: x, y, xystep
+
+    xystep = 1.0 / real(n - 1)
+    do i = 2, n - 1
+      do j = 2, n - 1
+        g_matrix(i, j) = g(xystep*(i - 1), xystep*(j - 1))
+      end do
+    end do
 
   end subroutine build_g_matrix
+
+  real function g(x, y)
+    real(8) :: x, y, pi
+    pi = 3.14
+    g = (1/(5**3*(2*pi)**(1/3)))*exp((-(20*x-10)**2-(20*y-10)**2)/(2*5**2))
+  end function g
 
 end program main
